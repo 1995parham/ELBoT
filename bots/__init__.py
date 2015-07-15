cda377c4e59a849010aa7cafe86638b09b84b76f
Parham Alvani <parham.alvani@gmail.com>
2015-07-15 20:52:43 +0430

    bots package is added.

---
+++ bots/__init__.py
@@ -0,0 +1,11 @@
+# In The Name Of God
+# ========================================
+# [] File Name : __init__.py
+#
+# [] Creation Date : 27-07-2015
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
+__author__ = 'Parham Alvani'
+
+from bots import python_bot, ftp_bot, ser_bot, wiki_bot, history_bot
