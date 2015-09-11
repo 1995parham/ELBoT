1f3556caa6d175dda9a4e8fa306f88830a89a64f
Parham Alvani <parham.alvani@gmail.com>
2015-09-11 07:05:13 +0430

    Documentation was improved.

--- elbot/core/BotFather.py
+++ elbot/core/BotFather.py
@@ -75,7 +75,7 @@ class BotFather(threading.Thread):
                        its update_id.
         :param limit: Limits the number of updates to be retrieved. Values between 1—100 are accepted. Defaults to 100
         :param timeout: Timeout in seconds for long polling. Defaults to 0, i.e. usual short polling
-        :return: []
+        :return: list[Update.Update]
         """
         updates = []
         params = {}
