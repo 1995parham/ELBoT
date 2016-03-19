8dee2c8858727d6633aef768c97a1be0dacb388b
Parham Alvani <parham.alvani@gmail.com>
2016-03-19 11:55:19 +0330

    Create domain package.

---
+++ elbot/domain/__init__.py
@@ -0,0 +1,8 @@
+# In The Name Of God
+# ========================================
+# [] File Name : __init__.py
+#
+# [] Creation Date : 3/19/16
+#
+# [] Created By : Parham Alvani (parham.alvani@gmail.com)
+# =======================================
