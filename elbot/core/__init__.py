8f69429e78a9874e6f0846a1122a68914c3a0a1d
Parham Alvani <parham.alvani@gmail.com>
2016-10-11 02:38:06 +0330

    Removes redundant information form module __init__ files.

--- elbot/core/__init__.py
+++
@@ -1,8 +0,0 @@
-# In The Name Of God
-# ========================================
-# [] File Name : __init__.py
-#
-# [] Creation Date : 09-09-2015
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
--- elbot/domain/__init__.py
+++
@@ -1,8 +0,0 @@
-# In The Name Of God
-# ========================================
-# [] File Name : __init__.py
-#
-# [] Creation Date : 3/19/16
-#
-# [] Created By : Parham Alvani (parham.alvani@gmail.com)
-# =======================================
