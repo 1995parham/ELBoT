2d2fb95eceaa9c1bb79e2164d1b5a3a0185241af
Parham Alvani <parham.alvani@gmail.com>
2017-01-08 02:38:50 +0330

    Serbot was improved and completed.

--- ser_bot.py
+++ ser_bot.py
@@ -8,7 +8,8 @@ elbot = ELBot('152389518:AAHW5iTAkK-3UqHV70PGd--bLQDdv3SnyQk')
 
 
 @elbot.message(r'^/ser*')
-def run_query():
-    print("seriously")
+def ser_handler(m: Message):
+    print("seriously wanted from %s" % m.src.first_name)
+    elbot.send_message(m.chat.id, 'seriously')
 
 elbot.run()
