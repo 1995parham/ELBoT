c111ca7473d148d4d9327b4bdce808aabd2f8e41
Parham Alvani <parham.alvani@gmail.com>
2017-01-08 03:16:44 +0330

    Ser bot was improved.

--- ser_bot.py
+++ ser_bot.py
@@ -7,7 +7,7 @@ from elbot.domain.message import Message
 elbot = ELBot('152389518:AAHW5iTAkK-3UqHV70PGd--bLQDdv3SnyQk')
 
 
-@elbot.message(r'^/ser*')
+@elbot.message(r'^/ser')
 def ser_handler(m: Message):
     print("seriously wanted from %s" % m.src.first_name)
     elbot.send_message(m.chat.id, 'seriously')
